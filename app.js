c08e2915bfc08a1635facaf2d1560c3ae0993f9c
raghadasfour-02 <r2asfour@uwaterloo.ca>
2022-01-02 22:13:48 -0500

    Responsive Navigation Bar
    Implement a responsive navigation bar that will readjust to a hamburger icon when viewed on mobile app.

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const logo = document.querySelectorAll("#logo path");
 for(let i = 0; i<logo.length; i++){
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function TopNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
